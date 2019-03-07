dee69213b8704058cd14476821d84239689645e0
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-07 11:02:17 +0100

    continuation

---
+++ regexsearch.py
@@ -0,0 +1,15 @@
+#! python3
+
+# A program that opens all files in the folder, search for user supplied regular expression and print the result to the screen.
+
+import os
+
+# TODO: Write regular expression.
+
+# TODO: Open new file to save the result.
+
+# TODO: Open all files.
+
+# TODO: Search files for regular expressions and save it to a new file.
+
+# TODO: Print the file with result to the screen.
