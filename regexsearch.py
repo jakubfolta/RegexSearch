e3e6a570c650f3cc6cc0c0cf383364a40555927a
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-08 23:03:02 +0100

    continuation

--- regexsearch.py
+++ regexsearch.py
@@ -6,21 +6,21 @@ import os
 import re
 
 # Write regular expression.
-regex = re.compile(r'\s(\w')
+regex = re.compile(r'\s\w\w\w\w\s')
 
 # Open new file to save the result.
 with open('result.txt', 'w') as result:
+
 # Change directory to folder with files to check.
     os.chdir('C:\\Users\ogi-8\Desktop\PythonProjects\RegexSearch\FilesToSearch')
-    print(os.listdir('C:\\Users\ogi-8\Desktop\PythonProjects\RegexSearch\FilesToSearch'))
-    print(os.getcwd())
+
 # TODO: Search files for regular expressions in a for loop and save the match to a new file.
     for file in os.listdir(os.getcwd()):
         print(file)
         text_file = open(file)
-        print(regex.findall(text_file.read()))
-        #print(matches)
-        #result.write(regex.findall(text_file.read()))
+        matches = '-'.join(regex.findall(text_file.read()))
+        print(matches)
+        result.write(matches)
 
 
 # TODO: Print the file with result to the screen.
